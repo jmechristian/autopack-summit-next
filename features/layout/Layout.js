3bf047bafe0796833371df50c3141a172d4f8410
jmechristian <jmechristian@gmail.com>
2025-11-13 07:26:12 -0500

    ssssss

--- features/layout/Layout.js
+++ features/layout/Layout.js
@@ -42,7 +42,7 @@ const Layout = ({ client, children }) => {
             transition={{ delay: 1.5, ease: 'easeInOut', duration: 1 }}
           >
             <motion.div className='font-medium text-ap-yellow text-lg leading-none uppercase font-oswald'>
-              Oct 15-17, 2025
+              Sep 30 - Oct 2, 2026
             </motion.div>
             <motion.div className='text-white/80 text-lg font-oswald'>
               Hyatt Regency, Greenville SC
